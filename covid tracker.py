6d295681c76c0a20959f9758ef90088362abc207
Saikrishna <83684269+SAIKRISHNA545@users.noreply.github.com>
2021-11-28 20:48:49 +0530

    Update covid tracker.py

--- covid tracker.py
+++ covid tracker.py
@@ -1,36 +1,21 @@
 from covid import Covid
-A=Covid()
-print(A.get_data())
-for j in range(1,10000):
-    act=A.get_total_active_cases()
-    rec=A.get_total_recovered()
-    death=A.get_total_deaths()
-    con=A.get_total_confirmed_cases()
-    print("1.GLOBAL COUNT")
-    print("2.ACTIVE CASES")
-    print("3.CONFIRMED CASES")
-    print("4.RECORVED CASES")
-    print("5.DECREAsED CASES")
-    print("6.Get Covid Update By country Names")
-    print("----------------------------------------------------")
-    x=input("enter your choice:")
-    if x=='1':
-        print("ACTIVE CASES:",act,"\nCONFIRMED CASES:",con,"\nRECOVERED CASES:",rec,"\nDECREASED CASES:",death)
-    elif x=='2':
-        print("ACTIVE CASES:",act)
-    elif x=='3':
-        print("CONFIRMED CASES:",con)
-    elif x=='4':
-        print("RECORVED CASES:",rec)
-    elif x=='5':
-        print("DECREASED CASES:",death)
-    elif x=='6':
-        y=input("enter country name:")
-        C=A.get_status_by_country_name(y)
-        print(C)
-    else:
-        print("invalid input")
-        break
-        
 
-
+covid = Covid()
+print("1.Conformed cases 2.Active cases 3.Recover count")
+x=int(input("enter your choice:"))
+if x==1:
+    confirmed = covid.get_total_confirmed_cases()
+    print('Confirmed :', end =" ")
+    print(confirmed)
+elif x==2:
+    active = covid.get_total_active_cases()
+    print("Active:", end=" ")
+    print(active)
+elif x==3:
+    recovered = covid.get_total_recovered()
+    print('Recovered:', end =" ")
+    print(recovered)
+else:
+    deaths = covid.get_total_deaths()
+    print('Deaths:', end =" ")
+    print(deaths)
